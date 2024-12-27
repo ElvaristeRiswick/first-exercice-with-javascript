eb5549da13a9caf13c9da22f2fa8cba2d8fbd1c5
Riswick <riswickelvariste@gmail.com>
2024-12-27 08:49:18 -0400

    exercice js

---
+++ variable.js
@@ -0,0 +1,21 @@
+"Hollo worlds";
+//Sconsole.log("hello wordsS");
+
+let salutation = "hello words ";
+//console.log(salutation);
+
+//let nom="string"
+//let age=10
+//let isStudent="true"
+
+let nom = "Djimelove";
+let age = 22;
+let isStudent = true;
+console.log(
+  "hello" + " " + nom + "tu as" + " " + age + "ans" + " " + isStudent
+);
+
+const ville1 = "la grande ville de ouanaminthe est belle ";
+const ville2 = "la grand ville de forliberte est belle aussi";
+
+//console.log(ville1 + " " + ville2);
